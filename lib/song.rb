63bf6663c0ce8f86e633b35d237b4cb2ade30b9b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-01 11:54:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,4 +28,11 @@ class Song
   def self.find_by_name(name)
     self.all.detect{|song| song.name == name }
   end
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) != nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
 end
