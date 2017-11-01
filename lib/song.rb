a4ad93d7531bb3ab0c710f591173e02a79c914d8
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-01 12:17:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,8 @@ class Song
   def self.new_from_filename(name)
     array_split = name.split(/[-.]/)
     song = self.create
-    song.name = array_split[0]
-    song.artist_name = array_split[1]
+    song.name = array_split[0].strip
+    song.artist_name = array_split[1].strip
     song
   end
 end
